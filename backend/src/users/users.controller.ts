792f91f72b54db982cd71d556267ac6926036105
Pedro Bueno <pedroarcbueno@gmail.com>
2026-02-20 17:08:39 -0300

    back:feat: Adding swagger documentation

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,34 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Controller, Get, Post, Body, Param, Delete } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: "Cria um novo usuário" })
+  @ApiResponse({ status: 201, description: "Usuário criado com sucesso." })
+  @ApiResponse({ status: 409, description: "Email já cadastrado." })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Lista todos os usuários" })
+  @ApiResponse({ status: 200, description: "Lista retornada com sucesso." })
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Delete(":id")
+  @ApiOperation({ summary: "Remove um usuário por ID" })
+  @ApiResponse({ status: 200, description: "Usuário removido com sucesso." })
+  @ApiResponse({ status: 404, description: "Usuário não encontrado." })
+  remove(@Param("id") id: string) {
+    return this.usersService.remove(id);
   }
 }
