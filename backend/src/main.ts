d020c7ed2b79be51da29a800a55ff73312bdc3e7
Pedro Bueno <pedroarcbueno@gmail.com>
2026-02-20 17:02:56 -0300

    back:feat: Implementing Swagger interface

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Ilia Contacts API")
+    .setDescription("The Ilia Take Home API description")
+    .setVersion("1.0")
+    .addTag("users")
+    .build();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +20,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
